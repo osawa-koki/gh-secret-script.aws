871ff3d1b1fb5a65f6cd0980c392107e1f546f52
osawa-koki <github@osawa.dev>
2023-02-26 15:38:45 +0900

    小さな修正。

--- gh-aws-secret.ps1
+++ gh-aws-secret.ps1
@@ -1,12 +1,13 @@
 
 # 配列を作成
-$envs = @(
+$secret_keys = @(
   "AWS_ACCESS_KEY_ID"
   "AWS_SECRET_ACCESS_KEY"
   "AWS_DEFAULT_REGION"
 )
 
 # ループ
-foreach ($env in $envs) {
-  gh secret set $env --body $env
+foreach ($secret_key in $secret_keys) {
+  $secret_value = [Environment]::GetEnvironmentVariable($secret_key)
+  gh secret set $secret_key --body $secret_value
 }
